a0195bf56fb6834b9eb69db0b41c203b9e8e0492
Vince Bello <vince@entelo.com>
2018-06-04 13:19:25 -0700

    App and API

--- api/config/initializers/new_framework_defaults.rb
+++ api/config/initializers/new_framework_defaults.rb
@@ -2,7 +2,9 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
-# Read the Rails 5.0 release notes for more info on each option.
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
+
+Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
