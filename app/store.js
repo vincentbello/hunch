1cd60438829a24eba1f8025b13fca0965a21d6b8
Vince Bello <vince@entelo.com>
2018-07-01 23:38:07 -0700

    Devtools setup

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 // @flow
 import { createStore, applyMiddleware, compose } from 'redux';
 import { middleware as reduxPackMiddleware } from 'redux-pack';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from 'reducers';
@@ -16,10 +17,10 @@ if (__DEV__) {
   middleware = [...middleware, createLogger()];
 }
 
-const composedEnhancers = compose(
+const composedEnhancers = composeWithDevTools(
   applyMiddleware(...middleware),
   ...enhancers
 );
 
-const store = createStore(rootReducer, initialState, composedEnhancers);
+const store = createStore(rootReducer, composedEnhancers);
 export default store;
