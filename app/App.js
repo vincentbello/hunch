e984991a590a1687286911cd0bb512b2d07e0863
Vince Bello <vince@entelo.com>
2018-07-01 21:02:30 -0700

    Redux setup

--- app/App.js
+++ app/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
+import store from './store';
+
+export default class App extends React.Component<{}> {
+  render(): React.Node {
     return (
-      <View style={styles.container}>
-        <Text>Hello World! Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Hello World! Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
