bb44b9daf0b72e5fe967fdeff91d25143b34c5da
Vince Bello <vince@entelo.com>
2018-07-14 20:14:38 -0700

    FB login

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,8 +1,11 @@
 // @flow
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Linking, View, Text, TouchableOpacity } from 'react-native';
 
 import { connect } from 'react-redux';
+import { stringify } from 'query-string';
+
+import { FB_APP_ID } from 'constants/third-party';
 
 import { SocialIcon } from 'react-native-elements';
 
@@ -17,11 +20,29 @@ const mapStateToProps = () => ({});
 const mapDispatchToProps = {};
 
 class LoginContainer extends React.Component<{}> {
+  componentDidMount() {
+    Linking.addEventListener('url', this.handleURL);
+  }
+
+  handleURL = (event) => {
+    console.log(event.url);
+  };
+
+  loginToFacebook = () => {
+    const params = stringify({
+      response_type: 'token',
+      client_id: FB_APP_ID,
+      redirect_uri: `fb${FB_APP_ID}://authorize$scope=email`,
+    });
+    console.log('params', params);
+    Linking.openURL(`https://graph.facebook.com/oauth/authorize?${params}`);
+  };
+
   render(): React.Node {
     return (
       <View style={SplashStyles}>
         <Text style={{ ...Typography.h1, marginBottom: 16 }}>Welcome to Hunch!</Text>
-        <TouchableOpacity onPress={() => console.log('FB LOGIN')}>
+        <TouchableOpacity onPress={this.loginToFacebook}>
           <SocialIcon
             button
             style={{ borderRadius: 4, padding: 16 }}
