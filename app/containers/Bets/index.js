6e10fc153875b9802f4f2cbb9adedbc4af90870a
Vince Bello <vince@entelo.com>
2018-09-28 08:01:34 -0700

    Bets list start

--- app/containers/Bets/index.js
+++ app/containers/Bets/index.js
@@ -1,20 +1,19 @@
 // @flow
 import * as React from 'react';
-import { AsyncStorage, Linking, View, Text, TouchableOpacity } from 'react-native';
+import { ActivityIndicator, FlatList, Image, StyleSheet, View, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { getBets } from 'selectors/bets';
 import { fetchBets } from 'actions/bets';
+import Colors from 'theme/colors';
 
 import { type Bet } from 'types/bet';
 import { type Action, type PromiseState } from 'types/redux';
 import { type State as ReduxState } from 'types/state';
 import { type ReduxState as UserState } from 'reducers/user';
 
-import Colors from 'theme/colors';
 import { SplashStyles } from 'theme/app';
 import Typography from 'theme/typography';
 
@@ -42,16 +41,75 @@ type Props = ReduxProps & {
   },
 };
 
+const styles = StyleSheet.create({
+  Bets: {
+    flex: 1,
+  },
+  Bets_splash: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Bets__list: {
+    marginTop: 8,
+  },
+  Bets__item: {
+    flexDirection: 'row',
+    height: 76,
+    backgroundColor: 'white',
+    borderRadius: 2,
+    marginLeft: 8,
+    marginRight: 8,
+    marginBottom: 8,
+    padding: 8,
+    alignItems: 'center',
+  },
+  Bets__itemImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginRight: 8,
+  },
+  Bets__itemContent: {
+    flex: 1,
+  },
+  Bets__itemMeta: {
+    fontSize: 14,
+    color: 'gray',
+  },
+});
+
 class BetsContainer extends React.Component<Props> {
   componentWillMount() {
-    this.props.actions.fetchBets('active');
+    this.fetchBets();
   }
+
+  fetchBets = (): void => this.props.actions.fetchBets('active');
+
+  renderBets = (): React.Node => this.props.bets.data !== null && (
+    <FlatList
+      style={styles.Bets__list}
+      data={this.props.bets.data}
+      keyExtractor={(bet: Bet): string => `${bet.id}`}
+      onRefresh={this.fetchBets}
+      refreshing={this.props.bets.isLoading}
+      renderItem={({ item: bet }): React.Node => (
+        <View style={styles.Bets__item}>
+          <Image style={styles.Bets__itemImage} source={{ uri: this.props.user.data.imageUrl }} />
+          <View style={styles.Bet__itemContent}>
+            <Text styles={styles.Bet__itemMeta}>{bet.wager}</Text>
+          </View>
+        </View>
+      )}
+    />
+  );
 
   render(): React.Node {
     const { bets } = this.props;
     return (
-      <View>
-        <Text>{bets.data ? bets.data.length : 'Loading'} Bets</Text>
+      <View style={[styles.Bets, bets.isLoading && styles.Bets_splash]}>
+        {bets.isLoading ? (
+          <ActivityIndicator size="large" color={Colors.brand.primary} />
+        ) : this.renderBets()}
       </View>
     );
   }
