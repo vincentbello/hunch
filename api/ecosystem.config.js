25f192e6e63d74c9ea76c5bd440ac9d51ae19f6b
Vincent Bello <vincent.bello@gmail.com>
2018-11-13 23:18:03 -0800

    PM2 config

---
+++ api/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: 'Hunch',
+    script: './dist/bin/www',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    // env: {
+    //   NODE_ENV: 'development'
+    // },
+    // env_production: {
+    //   NODE_ENV: 'production'
+    // }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ec2-user',
+      host : 'ec2-54-200-177-87.us-west-2.compute.amazonaws.com',
+      key  : '~/.ssh/hunch_instance_key.pem',
+      ref  : 'origin/master',
+      repo : 'git@github.com:vincentbello/hunch.git',
+      path : '/home/ec2-user/hunch',
+      'post-deploy' : 'cd api && yarn && cp ~/.hunch_env .env && npm run build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
