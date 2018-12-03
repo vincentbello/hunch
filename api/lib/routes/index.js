63fa81a08e4dd4df2b4205d3f2fec2885f57e800
Vincent Bello <vincent.bello@gmail.com>
2018-12-02 17:05:12 -0800

    Privacy policy served in Node

--- api/lib/routes/index.js
+++ api/lib/routes/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import path from 'path';
 import Sequelize, { Op } from 'sequelize';
 import models from '../db/models';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.sendStatus(200);
+});
+
+/** GET privacy policy. */
+router.get('/privacy', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'privacy_policy.html'));
 });
 
 export default router;
